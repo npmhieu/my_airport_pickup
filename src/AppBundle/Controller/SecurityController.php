b7d64108fac4a2cee4d5192a05d13b25f3a03a03
TrucNguyen <kapla3112@users.noreply.github.com>
2019-09-25 17:15:52 +0200

    update register

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -5,6 +5,9 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Routing\Annotation\Route;
+use AppBundle\Entity\User;
+use AppBundle\Form\UserForm;
+use Symfony\Component\HttpFoundation\Request;
 
 class SecurityController extends Controller
 {
@@ -22,6 +25,41 @@ class SecurityController extends Controller
     return $this->render('security/login.html.twig', [
       'last_username' => $lastUsername,
       'error'         => $error,
+    ]);
+  }
+  /**
+   * @Route("/register", name="register")
+   */
+  public function registerAction(Request $request)
+  {
+    $user = new User();
+
+    $form = $this->createForm(UserForm::class, $user);
+    $form->handleRequest( $request );
+
+    if ( $form->isSubmitted() && $form->isValid()) {
+      try {
+
+        $user = $form->getData();
+        $user->setRole(4);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        // Flash bag
+
+        $this->addFlash('success', 'Create User successfully!');
+        return $this->redirectToRoute('homepage');
+
+      } catch (\Exception $ex) {
+
+        $this->addFlash('error', 'Something went wrong. ' . $ex->getMessage());
+        $this->container->get('logger')->error($ex->getMessage());
+      }
+    }
+    return $this->render('security/register.html.twig', [
+      'userForm' => $form->createView()
     ]);
   }
 }
---
+++ src/AppBundle/Form/UserForm.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace AppBundle\Form;
+
+
+use AppBundle\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+
+class UserForm extends AbstractType
+{
+  public function buildForm(FormBuilderInterface $builder, array $options)
+  {
+
+    $builder
+      ->add(
+        'fullname',
+        TextType::class,
+        array(
+          'label' => 'Full Name:',
+          'attr' => array('class' => 'col-md-10 form-control'),
+          'label_attr' => array('class' => 'col-md-2 col-form-label'),
+        )
+      )
+      ->add(
+        'email',
+        TextType::class,
+        array(
+          'label' => "Email:",
+          'attr' => array('class' => 'col-md-10 form-control'),
+          'label_attr' => array('class' => 'col-md-2 col-form-label'),
+          'required' => true,
+        )
+      )
+      ->add(
+        'phone',
+        TextType::class,
+        array(
+          'label' => "Phone:",
+          'attr' => array('class' => 'col-md-10 form-control arrive-date'),
+          'label_attr' => array('class' => 'col-md-2 col-form-label'),
+          'required' => true,
+        )
+      )
+
+      ->add(
+        'password',
+        TextType::class,
+        array(
+          'label' => "Password:",
+          'attr' => array('class' => 'col-md-10 form-control'),
+          'label_attr' => array('class' => 'col-md-2 col-form-label'),
+          'required' => true,
+        )
+      )
+
+
+
+
+      ->add(
+        'register',
+        SubmitType::class,
+        array(
+          'label' => 'Create Account',
+          'attr' => array('class' => 'col-md-1 btn btn-primary mt-3'),
+        )
+      );
+  }
+
+  public function configureOptions(OptionsResolver $resolver)
+  {
+    $resolver->setDefaults(
+      array(
+        'data-class' => User::class,
+      )
+    );
+  }
+
+  public function getName()
+  {
+    return 'create_User';
+  }
+}
